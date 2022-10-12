f574746f0581c0c9ec3cadae01b8cb88570b970a
Bwade808 <bwka85@gmail.com>
2022-10-12 13:08:35 -0700

    troubleshooting edit v1.1

--- back-end/server.js
+++ back-end/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const { Client } = require('pg');
-const config = require('./config.js')[process.env.NODE_ENV || "dev"];
+const config = require('./config')[process.env.NODE_ENV || "dev"];
 const PORT = config.port;
 const client = new Client ({
     connectionString: config.connectionString,
