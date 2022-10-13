d8cbe88e23a67f51402f9cd3b46a0d826939eef9
Bwade808 <bwka85@gmail.com>
2022-10-13 13:35:05 -0700

    adding refactored code

--- back-end/server.js
+++ back-end/server.js
@@ -11,50 +11,50 @@ client.connect();
 
 app.use(express.json());
 app.use(cors());
-
-
-
 // const corsOptions = {
 //     origin: "https://phoenix-event-finder.onrender.com/",
 //     optionsSuccessStatus: 200
 // };
 
+//SEARCH ALL ROUTE
 app.get('/events/october', (req, res)=>{
     client.query('SELECT * FROM october_events')
     .then(result =>{
         res.send(result.rows);
     })
     .catch(err => {
-        res.console.log(err);
+        console.log(err);
     })
 });
 
+//SEARCH QUERY ROUTE
 app.get('/events/october/:type', (req, res)=>{
     let type = req.params.type;
-    let month = req.params.month;
-    
-    client.query('SELECT * FROM october_events WHERE event_type=$1', [type])
-    .then(result => {
-        res.send(result.rows);
-    })
-    .catch(err => {
-        res.console.log(err);
-    })
-})
+    // let month = req.params.month;
+        client.query('SELECT * FROM october_events WHERE event_type=$1', [type])
+        .then(result => {
+            res.send(result.rows);
+        })
+        .catch(err => {
+            console.log(err);
+        })
+});
 
+//POST NEW EVENT ROUTE
 app.post('/events/october', (req, res) => {
     let newEvent = req.body;
-    let month = req.params.month;
+    // let month = req.params.month;
     client.query('INSERT INTO october_events (event_name, event_type, month_day) VALUES ($1, $2, $3)', 
     [newEvent.event_name, newEvent.event_type, newEvent.month_day])
     .then(result => {
         res.send(result.rows)
     })
     .catch(err => {
-        res.console.log(err);
+        console.log(err);
     })
-})
+});
 
+//DELETE EVENT ROUTE
 app.delete('/events/october/:id', (req, res) => {
     let eventId = req.params.id;
     console.log(eventId);
@@ -63,12 +63,11 @@ app.delete('/events/october/:id', (req, res) => {
         res.send(result.rows)
     })
     .catch(err => {
-        res.console.log(err);
+        console.log(err);
     })
-})
+});
 
-
-
+//Catch all 404 error handler
 app.use((req, res, next)=>{
     res.status(404).send('Not found')
 });
--- front-end/public/index.js
+++ front-end/public/index.js
@@ -10,147 +10,128 @@ const aType = document.querySelector('#add_event_type');
 const day = document.querySelector('#event_date');
 const eName = document.querySelector('#e_name');
 const clickCard = document.querySelectorAll('.newCard');
+let deleteIcon;
 
-const url = "https://event-finder-api.onrender.com";
-// const url = "http://localhost:8002";
+//RENDER and LOCALHOST URLs
+    // const url = "https://event-finder-api.onrender.com";
+    const url = "http://localhost:8002";
 
-// const cardEvent = document.querySelector('.cardEvent');
-
-// let cardCreator = (name, type, date) => {
-//     let newCard = document.createElement('div');
-//     newCard.className = "newCard";
-//     let eventName = document.createElement('h2');
-//     eventName.className = "cardEvent";
-//     eventName.textContent = name;
-//     let eventTypeDate = document.createElement('h3');
-//     eventTypeDate.className = "cardTypeDate";
-//     eventTypeDate.textContent = `${type} / October ${date}, 2022`;
-//     newCard.append(eventName, eventTypeDate);
-//     return newCard;
-//     // results.append(newCard);
-// };
-let querySearch = () => {
-    results.innerHTML = "";
-    let typeVal = sType.value;
-    let monthVal = sMonth.value;
-    fetch(`${url}/events/${monthVal}/${typeVal}`)
-    .then((response) => response.json())
-    .then((data) => {
-        return data;
-    })
-    .then((data) => {
-        for (var i = 0; i < data.length; i++){
-            let data1 = data[i].event_name;
-            let data2 = data[i].event_type;
-            let data3 = data[i].month_day;
-            let newCard = document.createElement('div');
-            newCard.className = "newCard";
-            newCard.id = data[i].event_id;
-            let eventName = document.createElement('h2');
-            eventName.className = "cardEvent";
-            eventName.textContent = data1;
-            let eventTypeDate = document.createElement('h3');
-            eventTypeDate.className = "cardTypeDate";
-            eventTypeDate.textContent = `${data2} / October ${data3}, 2022`;
-            let deleteIcon = document.createElement('button');
-            deleteIcon.className = "fa fa-trash";
-            deleteIcon.id = data[i].event_id;
-            let updateIcon = document.createElement('button');
-            updateIcon.className = "fas fa-edit";
-            newCard.append(eventName, eventTypeDate, deleteIcon, updateIcon);
-            results.append(newCard);
-
-            deleteIcon.addEventListener('click', (e) => {
-                console.log(e.target.id)
-                let eId = e.target.id;
-                let isConfirmed = confirm('Are you sure you want to delete this event?');
-                if(isConfirmed){
-                    fetch(`${url}/events/october/${eId}`,{
-                        method: 'DELETE',
-                        headers: {
-                            'Content-Type': 'application/json'
-                        }
-                    })
-                    .then((data) => {
-                        alert('You have successfully deleted an event');
-                        console.log('Successfully delete', data)
-                    })
-                }
-            });
-        }
-    })
-};
-
-let searchAll = () =>{
-    resultsH2.innerHTML = h2Text;
-if(sMonth.value === "october" && sType.value === "All"){
-fetch(`${url}/events/october`)
-.then((response)=> response.json())
-.then((data)=>{
-    return data;
-})
-.then((data)=>{
-    for (var i = 0; i < data.length; i++){
-        let data1 = data[i].event_name;
-        let data2 = data[i].event_type;
-        let data3 = data[i].month_day;
+//FUNCTION TO CREATE RESULT CARDS
+let cardCreator = (name, type, date, data) => {
         let newCard = document.createElement('div');
         newCard.className = "newCard";
-        newCard.id = data[i].event_id;
+        newCard.id = data.event_id;
         let eventName = document.createElement('h2');
         eventName.className = "cardEvent";
-        eventName.textContent = data1;
+        eventName.textContent = name;
         let eventTypeDate = document.createElement('h3');
         eventTypeDate.className = "cardTypeDate";
-        eventTypeDate.textContent = `${data2} / October ${data3}, 2022`;
-        let deleteIcon = document.createElement('button');
+        eventTypeDate.textContent = `${type} / October ${date}, 2022`;
+        deleteIcon = document.createElement('button');
         deleteIcon.className = "fa fa-trash";
+        deleteIcon.id = data.event_id;
         let updateIcon = document.createElement('button');
         updateIcon.className = "fas fa-edit";
         newCard.append(eventName, eventTypeDate, deleteIcon, updateIcon);
-        results.append(newCard);
-    }
-});
-} else {
-    querySearch();
-}
+        return newCard;
 };
 
+//FUNCTION TO QUERY SEARCH EVENT TYPES in OCTOBER
+let querySearch = () => {
+        results.innerHTML = "";
+        let typeVal = sType.value;
+        let monthVal = sMonth.value;
+        // if(sMonth.value === "october" && sType.value === "All"){
+        fetch(`${url}/events/${monthVal}/${typeVal}`)
+        .then((response) => response.json())
+        .then((data) => {
+            return data;
+        })
+        .then((data) => {
+            for (var i = 0; i < data.length; i++){
+                let data1 = data[i].event_name;
+                let data2 = data[i].event_type;
+                let data3 = data[i].month_day;
+                let dataInd = data[i];
+                let createCard = cardCreator(data1, data2, data3, dataInd);
+                results.append(createCard);
+            //DELETE ICON EVENT LISTENER
+                deleteIcon.addEventListener('click', (e) => {
+                    console.log(e.target.id)
+                    let eId = e.target.id;
+                    let isConfirmed = confirm('Are you sure you want to delete this event?');
+                    if(isConfirmed){
+                        fetch(`${url}/events/october/${eId}`,{
+                            method: 'DELETE',
+                            headers: {
+                                'Content-Type': 'application/json'
+                            }
+                        })
+                        .then((data) => {
+                            alert('You have successfully deleted an event');
+                            console.log('Successfully delete', data)
+                        })
+                    }
+                });
+            }
+        })
+};
 
+//FUNCTION TO SEARCH ALL of OCTOBER
+let searchAll = () => {
+        results.innerHTML = "";
+        resultsH2.innerHTML = h2Text;
+        if(sMonth.value === "october" && sType.value === "All"){
+        fetch(`${url}/events/october`)
+        .then((response)=> response.json())
+        .then((data)=>{
+            return data;
+        })
+        .then((data)=>{
+            for (var i = 0; i < data.length; i++){
+                    let data1 = data[i].event_name;
+                    let data2 = data[i].event_type;
+                    let data3 = data[i].month_day;
+                    let dataInd = data[i];
+                    let createCard = cardCreator(data1, data2, data3, dataInd);
+                    results.append(createCard);
+            }
+        });
+        } else {
+            querySearch();
+        }
+};
 
 //FETCH POST
 let addEvent = () => {
-    let aMonthVal = aMonth.value;
-    let newEvent = {
-        "event_name": eName.value,
-        "event_type": aType.value,
-        "month_day": Number(day.value.substring(8, 10))
-        
-    };
-    if(aMonthVal === "October"){
-        fetch(`${url}/events/october`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(newEvent)
-        })
-        .then((data) => {
-        alert('You have successfully added an event.  Thank you!')
-        console.log('Success', data)
-        })
-    };
+        let aMonthVal = aMonth.value;
+        let newEvent = {
+            "event_name": eName.value,
+            "event_type": aType.value,
+            "month_day": Number(day.value.substring(8, 10))
+        };
+        if(aMonthVal === "October"){
+            fetch(`${url}/events/october`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(newEvent)
+            })
+            .then((data) => {
+            alert('You have successfully added an event.  Thank you!')
+            console.log('Success', data)
+            })
+        };
 };
 
 
 
 
 
-// let logDate = () => {
-//     let day = document.querySelector('#event_date');
-//     let dayVal = day.value.substring(8, 10);
-//     console.log(dayVal);
-// }
+
+
+
 
 
 
